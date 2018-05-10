128f4a522ef43c5fe25dfad156a4745f46b24173
Evelyn <misscookiess@gmail.com>
2018-05-09 19:45:47 -0700

    "Header Render"

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Header } from './Header.js';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
