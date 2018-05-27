1a48593f3db2c2e93b3ed3840997250a72dd11a1
Evelyn <misscookiess@gmail.com>
2018-05-27 15:20:51 -0700

    toggle

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import {withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
-
+import {GoogleMap, withGoogleMap, withScriptjs} from "react-google-maps";
+import {Marker} from "./Marker";
 
 class Map extends React.Component {
+
     render() {
-        return(
+        const locations = [{lat: -34.397, lng: 150.044}, {lat: -34.297, lng: 150.344}, {lat: -34.197, lng: 150.644},];
+        return (
             <GoogleMap
                 defaultZoom={8}
-                defaultCenter={{ lat: -34.397, lng: 150.644 }}
+                defaultCenter={{lat: -34.397, lng: 150.644}}
             >
-                <Marker
-                    position={{ lat: -34.397, lng: 150.644 }}
-                >
-                </Marker>
+                {locations.map((loc) => <Marker locations={loc}/>)}
             </GoogleMap>
 
         );
